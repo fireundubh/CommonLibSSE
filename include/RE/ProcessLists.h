16c555b083288d5a98f7eecae2ec1d70fe0d4a9e
powerofthree <cruelflames678@gmail.com>
2019-12-30 23:05:09 +0530

    updated object handles for processlists/referenceeffect

--- include/RE/ProcessLists.h
+++ include/RE/ProcessLists.h
@@ -2,6 +2,7 @@
 
 #include "RE/BSTArray.h"
 #include "RE/BSTSingleton.h"
+#include "RE/BSPointerHandle.h"
 
 
 namespace RE
@@ -38,7 +39,7 @@ namespace RE
 		bool						enableDetection;				// 001
 		bool						enableDetectionStats;			// 002
 		UInt8						pad003;							// 003
-		RefHandle					trackedDetectionHandle;			// 004
+		ActorHandle					trackedDetectionHandle;			// 004
 		bool						enableHighProcessing;			// 008
 		bool						enableLowProcessing;			// 009
 		bool						enableMiddleHighProcessing;		// 00A
@@ -53,14 +54,14 @@ namespace RE
 		HANDLE						semaphore;						// 020
 		UInt32						unk028;							// 028
 		UInt32						pad02C;							// 02C
-		BSTArray<RefHandle>			highProcesses;					// 030
-		BSTArray<RefHandle>			lowProcesses;					// 048
-		BSTArray<RefHandle>			middleLowProcesses;				// 060
-		BSTArray<RefHandle>			middleHighProcesses;			// 078
-		BSTArray<RefHandle>*		highProcessesPtr;				// 090
-		BSTArray<RefHandle>*		lowProcessesPtr;				// 098
-		BSTArray<RefHandle>*		middleLowProcessesPtr;			// 0A0
-		BSTArray<RefHandle>*		middleHighProcessesPtr;			// 0A8
+		BSTArray<ActorHandle>		highProcesses;					// 030
+		BSTArray<ActorHandle>		lowProcesses;					// 048
+		BSTArray<ActorHandle>		middleLowProcesses;				// 060
+		BSTArray<ActorHandle>		middleHighProcesses;			// 078
+		BSTArray<ActorHandle>*		highProcessesPtr;				// 090
+		BSTArray<ActorHandle>*		lowProcessesPtr;				// 098
+		BSTArray<ActorHandle>*		middleLowProcessesPtr;			// 0A0
+		BSTArray<ActorHandle>*		middleHighProcessesPtr;			// 0A8
 		UInt64						unk0B0;							// 0B0
 		UInt64						unk0B8;							// 0B8
 		UInt64						unk0C0;							// 0C0
@@ -76,13 +77,13 @@ namespace RE
 		mutable BSUniqueLock		unk128Lock;						// 140
 		UInt64						unk148;							// 148
 		UInt64						unk150;							// 150
-		BSTArray<RefHandle>			unk158;							// 158
+		BSTArray<ActorHandle>		unk158;							// 158
 		UInt32						unk170;							// 170
 		UInt32						pad174;							// 174
 		UInt64						unk178;							// 178
 		BSTArray<void*>				unk180;							// 180
 		mutable BSUniqueLock		unk198;							// 198
-		BSTArray<RefHandle>			unk1A0;							// 1A0
+		BSTArray<ActorHandle>		unk1A0;							// 1A0
 		BSTArray<void*>				unk1B8;							// 1B8
 		float						unk1D0;							// 1D0
 		float						unk1D4;							// 1D4
