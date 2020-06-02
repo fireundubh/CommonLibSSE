71df13c7730a0d06367d3fda3482bc0f04e350f2
powerofthree <cruelflames678@gmail.com>
2020-06-03 03:39:42 +0530

    fixed filters

--- src/RE/FormComponents/Components/BGSBaseAlias/BGSBaseAlias.cpp
+++ src/RE/FormComponents/Components/BGSBaseAlias/BGSBaseAlias.cpp
@@ -13,4 +13,40 @@ namespace RE
 	{
 		return (flags & FLAGS::kQuestObject) != FLAGS::kNone;
 	}
+
+
+	bool BGSBaseAlias::IsEssential() const
+	{
+		return (flags & FLAGS::kEssential) != FLAGS::kNone;
+	}
+
+
+	bool BGSBaseAlias::IsProtected() const
+	{
+		return (flags & FLAGS::kProtected) != FLAGS::kNone;
+	}
+
+
+	void BGSBaseAlias::SetEssential()
+	{
+		flags |= FLAGS::kEssential;
+	}
+
+
+	void BGSBaseAlias::SetUnessential()
+	{
+		flags &= ~FLAGS::kEssential;
+	}
+
+
+	void BGSBaseAlias::SetProtected()
+	{
+		flags |= FLAGS::kProtected;
+	}
+
+
+	void BGSBaseAlias::SetUnprotected()
+	{
+		flags &= ~FLAGS::kProtected;
+	}
 }
