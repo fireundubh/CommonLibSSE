a08430ea22a87986fd0cd2b97fd7249ca37c21c2
crumpmasterjc <jcovey94@gmail.com>
2020-08-12 14:46:05 -0500

    Added Actor::RemoveSpell

--- src/RE/FormComponents/TESForm/TESObjectREFR/Actor/Actor.cpp
+++ src/RE/FormComponents/TESForm/TESObjectREFR/Actor/Actor.cpp
@@ -635,6 +635,12 @@ namespace RE
 		extraList.RemoveByType(ExtraDataType::kAttachedArrows3D);
 	}
 
+	bool Actor::RemoveSpell(SpellItem* a_spell)
+	{
+		using func_t = decltype(&Actor::RemoveSpell);
+		REL::Relocation<func_t> func{ REL::ID(37772) };
+		return func(this, a_spell);
+	}
 
 	bool Actor::RemoveSpell(SpellItem* a_spell)
 	{
